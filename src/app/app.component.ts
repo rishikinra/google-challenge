22d74661a2286430a267167183db228129e3052c
Rishi Kinra <rkinra@Rishis-MBP-2.lan>
2019-01-16 01:58:52 -0800

    all edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { Subscription, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'google-challenge';
+  myValue = 0;
+  disableButtons: boolean = false;
+
+  //listenerFn: () => void; // Render2 approach
+  subscription: Subscription; // RxJS approach
+
+  constructor(private renderer: Renderer2) {
+
+  }
+
+  ngOnDestroy() {
+    this.turnMouseTrackOff();
+  }
+
+  increment(event) {
+    ++this.myValue;
+  }
+
+  decrement(event){
+    --this.myValue;
+  }
+
+  turnMouseTrackOff() {
+    // Render2 approach.
+    //this.listenerFn(); // unsubscribe the document mouse move event
+    //this.listenerFn = undefined; // @NOTE: without doing this, the Fn still sticks around.
+
+    // RxJS approach.
+    this.subscription.unsubscribe();
+    this.subscription = undefined; // @NOTE: without doing this, the Fn still sticks around.
+
+    this.disableButtons = false;
+  }
+
+  toggleTrackMouseMove() {
+    //console.log(this.listenerFn);
+    console.log(this.subscription);
+
+    //if(this.listenerFn) {
+    if(this.subscription) {
+      this.turnMouseTrackOff();
+    } else {
+      this.disableButtons = true;
+
+      // Render2 approach.
+      //this.listenerFn = this.renderer.listen(document, 'mousemove', (event) => {
+      //  this.myValue = event.clientX;
+      //});
+
+      // RxJS approach.
+      this.subscription = fromEvent(document, 'mousemove')
+        .subscribe((event: MouseEvent) => {
+          this.myValue = event.clientX;
+       });
+
+   }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatButtonToggleModule,
+  MatFormFieldModule
+} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +17,20 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatFormFieldModule
+  ],
+  exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
